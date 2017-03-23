9d59cd6f661a804fd718322d26c43ca6abe04112
irsayasin <israambreenbwn@gmail.com>
2017-03-23 11:35:30 -0700

    Add files via upload

---
+++ Class6.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dell
+ */import java.util.Scanner;
+ 
+public class Class6 {
+    public static void main (String[] args)
+    {
+        Scanner scan=new Scanner(System.in)
+                String message;
+                System.out.println("Enter some text:");
+                
+                message=scan.nextLine();
+                System.out.ptintln("You entered:\"" +message+"")
+        
+        
+        
+        
+    }
+    
+}
