d57ede65dab5d111fa1e8cb1c597c85c5aeb0af7
Dell <Dell@DESKTOP-UA69GEB>
2017-03-23 07:36:05 -0700

    its my first program

---
+++ src/Book.java
@@ -0,0 +1,29 @@
+
+public class Book
+{
+    private int pages;
+    private String author;
+ 
+    private int chapters;
+
+    public Book()
+{
+pages=0;
+chapters=0;
+author="";
+            
+    
+}
+public void setpages(int p)
+{
+pages=p;
+}
+public void setchapters(int c)
+{
+chapters=c;
+}
+public void setauthor(String a)
+{
+author=a;
+}
+}
