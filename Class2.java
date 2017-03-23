6fd8ecc4e868f453292a091bd75b136184c2d4ce
irsayasin <israambreenbwn@gmail.com>
2017-03-23 11:33:47 -0700

    Add files via upload

---
+++ Class2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dell
+ */
+public class Class2 {
+    public static void main(String[] args)
+    { 
+        int A,B,C,D,E,Average;
+        A=10;
+        B=20;
+        C=30;
+        D=40;
+        E=50;
+        
+        Average=(A+B+C+D+E)/5;
+        
+        System.out.print("The Average is:="+Average);
+    
+}
