1afc8a5ad60b44578298a911ab225298228a5f05
irsayasin <israambreenbwn@gmail.com>
2017-03-23 11:34:39 -0700

    Add files via upload

---
+++ Class4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dell
+ */import java.util.Scanner;
+public class Class4 {
+    public static void main (String[] args)
+    {
+        Scanner M=new Scanner(System.in)
+                int marks;
+                 System.out.print("Enter your marks:=");
+                
+                marks=M.nextInt();
+                System.out.print("Your marks are:="+marks);
+    }
+}
