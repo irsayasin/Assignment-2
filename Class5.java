d5649dbf4621ac7ba7cf3a73dbe48aae25901222
irsayasin <israambreenbwn@gmail.com>
2017-03-23 11:35:05 -0700

    Add files via upload

---
+++ Class5.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dell
+ */import java.util.Scanner;
+ 
+public class Class5 {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        float height;
+        
+        System.out.print("Enter your height:=");
+        
+        height=scan.nextFloat();
+        
+        System.out.print("Your height is:="+height);
+    }
+    
+}
